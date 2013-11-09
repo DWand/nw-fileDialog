584fd53d3f0accb8eaba09a53c84b5c7477d684e
Max Prichinenko <max.prichinenko@gmail.com>
2013-11-09 23:20:31 +0200

    chore(callDialog): Simplifying
    Remove unnecessary code. It's not needed to reset a value of the input because we always
    create a new one.

--- nw-fileDialog.js
+++ nw-fileDialog.js
@@ -2,9 +2,7 @@ angular.module('DWand.nw-fileDialog', [])
 .factory('fileDialog', [function(){
   var callDialog = function(dialog, callback) {
     dialog.addEventListener('change', function() {
-      dialog.removeEventListener('change', arguments.callee, false);
       var result = dialog.value;
-      dialog.value = '';
       callback(result);
     }, false);
     dialog.click();
