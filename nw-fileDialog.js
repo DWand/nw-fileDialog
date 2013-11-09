b50f8c1b7aa1bb19be8f90b24ab1e3ed049dc1eb
Max Prichinenko <max.prichinenko@gmail.com>
2013-11-10 01:14:52 +0200

    Add comments about license and source

--- nw-fileDialog.js
+++ nw-fileDialog.js
@@ -1,3 +1,9 @@
+/* https://github.com/DWand/nw-fileDialog
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details. */
 angular.module('DWand.nw-fileDialog', [])
 .factory('fileDialog', [function(){
   var callDialog = function(dialog, callback) {
